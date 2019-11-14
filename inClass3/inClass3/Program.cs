601d9787abfc890b0ab6dce19162ead821b3750c
neilquizon <neil.quizon@gmail.com>
2019-11-14 10:25:37 -0800

    #1 Updated

--- inClass3/inClass3/Program.cs
+++ inClass3/inClass3/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Net;
+using System.Text;
 
 namespace inClass3
 {
@@ -16,10 +18,17 @@ namespace inClass3
                 Uri uri = new Uri(baseUrl + planets);
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+
+                Console.WriteLine(responseStream.ReadToEnd());
+
             }
             catch (Exception e)
             {
-
+                Console.WriteLine("An error has occured. Could not get planets:\n" + e.Message);
             }
+            Console.ReadLine();
+        }
     }
 }
+
