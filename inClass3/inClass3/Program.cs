c210a5683c499cb239d387748e6b4499a5c4471c
neilquizon <neil.quizon@gmail.com>
2019-11-14 10:18:09 -0800

    #1 Started inClass3

---
+++ inClass3/inClass3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace inClass3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string baseUrl = "https://swapi.co/api/";
+            string planets = "planets/";
+            string people = "people/";
+            try
+            {
+                Uri uri = new Uri(baseUrl + planets);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (Exception e)
+            {
+
+            }
+    }
+}
